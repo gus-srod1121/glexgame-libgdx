b2ddf52b86c020dadd0ccccaa2e92192dfc01cb2
gus-srod-bcit <gustavosrod1121@gmail.com>
2026-02-03 11:19:18 -0800

    Draw character on screen

--- core/src/main/java/io/github/glexgame/Main.java
+++ core/src/main/java/io/github/glexgame/Main.java
@@ -13,14 +13,14 @@ public class Main extends ApplicationAdapter {
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        image = new Texture("player.png");
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f); //fill color rgba(0.15, 0.15, 0.2, 1)
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(image, 0, 0);
         batch.end();
     }
 
